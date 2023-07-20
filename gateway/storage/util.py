8897871017da7ef70c119ffe73251b78ab393a4f
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-07-20 02:59:48 +0100

    clean and pick video queue from env variable

--- gateway/storage/util.py
+++ gateway/storage/util.py
@@ -1,17 +1,16 @@
-import pika, json
+import pika, json, os
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import PERSISTENT_DELIVERY_MODE
 from gridfs import GridFS
 from werkzeug.datastructures import FileStorage
 
+VIDEO_QUEUE = os.environ["VIDEO_QUEUE"]
+
 def upload(f: FileStorage, fs: GridFS, channel: BlockingChannel, access_payload):
     try:
-        print("now putting")
         file_id = fs.put(f)
-        print("finished putting")
     except Exception as err:
-        print(err)
-        return "internal server error", 500
+        return err
     
     message = {
         "video_id": str(file_id),
@@ -20,20 +19,16 @@ def upload(f: FileStorage, fs: GridFS, channel: BlockingChannel, access_payload)
     }
 
     try:
-        print("now publishing")
-        # TODO: video routing key should be placed in VIDEO_QUEUE os env
         channel.basic_publish(
             exchange="",
-            routing_key="video",
+            routing_key=VIDEO_QUEUE,
             body=json.dumps(message),
             properties=pika.BasicProperties(
                 delivery_mode=PERSISTENT_DELIVERY_MODE
             ),
         )
     except Exception as err:
-        print(err)
-        print("inside gateway storage")
         # entry point to initiate exponential backoff
         # and maybe log message in a DLQ or DLX
         # so that it is not forgotten.
-        pass
+        return err
