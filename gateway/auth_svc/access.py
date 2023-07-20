a3ee98571c0fd27c4a83f85761def82b3fdda05c
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-07-20 02:58:44 +0100

    add content-type header and use post body data

--- gateway/auth_svc/access.py
+++ gateway/auth_svc/access.py
@@ -3,15 +3,30 @@ from flask import Request
 from .utils import AUTH_SERVICE_ADDRESS
 
 def login(req: Request):
-    auth = req.authorization
+    auth = req.data
     if not auth:
         return None, ("missing credentials", 401)
 
-    basicAuth = (auth.username, auth.password)
-
     response = requests.post(
         f"http://{AUTH_SERVICE_ADDRESS}/login",
-        auth=basicAuth
+        headers={"Content-Type": "application/json"},
+        data=auth
+    )
+    
+    if response.status_code != 200:
+        return None, (response.text, response.status_code)
+    
+    return response.text, None
+
+def signup(req: Request):
+    auth = req.data
+    if not auth:
+        return None, ("missing credentials", 401)
+
+    response = requests.post(
+        f"http://{AUTH_SERVICE_ADDRESS}/signup",
+        headers={"Content-Type": "application/json"},
+        data=auth
     )
     
     if response.status_code != 200:
